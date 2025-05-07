45f3e332ece9b0062bfee01a307ac97931f8ec19
Nikhil Singh Mahara <nikesm9818@gmail.com>
2025-05-07 13:07:16 +0530

    new components added

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  loginObj: any = {
+    username: '',
+    password: ''
+  }
+
+
+  router = inject(Router)
+  onLogin(){
+    if(this.loginObj.username == "admin" && this.loginObj.password == "admin"){
+      this.router.navigateByUrl('dashboard')
+    } else{
+      alert("Wrong Creds")
+    }
+  }
+}
